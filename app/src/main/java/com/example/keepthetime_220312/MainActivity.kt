3c139248a7e0746b4266d65a583ad9b52ca1c81a
TFQ_Moon <no1msh1217@gmail.com>
2022-03-12 11:44:31 +0900

    DataBinding 사용방법 확인

--- app/src/main/java/com/example/keepthetime_220312/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_220312/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.keepthetime_220312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime_220312.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    binding : 어떤 xml을 접근하는지. 자료형으로 설정.
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this ,R.layout.activity_main)
+
     }
 }
