2ff6db5dc58afc428798bbf52d2aaeec5cffaa14
TFQ_Moon <no1msh1217@gmail.com>
2022-03-20 10:48:02 +0900

    DatePickerDialog 구현
    근데 '월' 표시가 이상하게 된다.

--- app/src/main/java/com/example/keepthetime_220312/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_220312/EditAppointmentActivity.kt
@@ -1,7 +1,9 @@
 package com.example.keepthetime_220312
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_220312.databinding.ActivityEditAppointmentBinding
 
@@ -17,6 +19,31 @@ class EditAppointmentActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.txtDate.setOnClickListener {
+
+//            날짜가 선택되면 할 일 저장
+
+            val dsl = object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    year, month, dayOfMonth = > 달력을 통해서 선택한 일자 정보
+                    Toast.makeText(mContext, "${year}년 ${month}월 ${dayOfMonth}일", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+//            실제로 달력 팝업 띄우기.
+
+        val dpd = DatePickerDialog(
+            mContext,
+            dsl,
+            2022,
+            3,
+            20
+        ).show()
+
+        }
 
     }
 
