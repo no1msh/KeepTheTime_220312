93d3cc85764406ba4cf42fd693432274d42d9e27
TFQ_Moon <no1msh1217@gmail.com>
2022-03-26 11:44:27 +0900

    ODSay 라이브러리의 대중교통길찾기 활용
     - API 문서의 필수값들만 대입 (출발 / 도착 좌표)

--- app/src/main/java/com/example/keepthetime_220312/ViewMapActivity.kt
+++ app/src/main/java/com/example/keepthetime_220312/ViewMapActivity.kt
@@ -8,6 +8,10 @@ import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
+import com.odsay.odsayandroidsdk.API
+import com.odsay.odsayandroidsdk.ODsayData
+import com.odsay.odsayandroidsdk.ODsayService
+import com.odsay.odsayandroidsdk.OnResultCallbackListener
 
 class ViewMapActivity : BaseActivity() {
 
@@ -55,21 +59,49 @@ class ViewMapActivity : BaseActivity() {
             marker.position = latLng
             marker.map = naverMap
 
-//            정보창 띄우기
+////            정보창 띄우기
+//
+//            val infoWindow = InfoWindow()
+//
+////            object : 추상클래스(생성자) = > 추상 클래스 객체를 담는다.
+////            object : 인터페이스 {    } = > 인터페이스는 생성자가 없이 사용한다.
+//            infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext) {
+//                override fun getText(p0: InfoWindow): CharSequence {
+////                    charSequence : String 으로 생각해도 무방.
+//                    return mAppointmentData.place
+//                }
+//
+//            }
+//
+//            infoWindow.open(marker)
 
-            val infoWindow = InfoWindow()
+//                대중교통 길찾기 라이브러리 활용 => 소요 시간 + 비용 정보창 띄우기.
 
-//            object : 추상클래스(생성자) = > 추상 클래스 객체를 담는다.
-//            object : 인터페이스 {    } = > 인터페이스는 생성자가 없이 사용한다.
-            infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext) {
-                override fun getText(p0: InfoWindow): CharSequence {
-//                    charSequence : String 으로 생각해도 무방.
-                    return "정보창 내용"
+            val odSay = ODsayService.init(mContext,"ik7tX98HxBmQ0FiXS7YdjDPOKvFQg1JyBUdOU0C3pFo")
+
+            odSay.requestSearchPubTransPath(
+                mAppointmentData.start_longitude.toString(), // 출발지 X좌표 (경도)를 String으로
+                mAppointmentData.start_latitude.toString(), // 출발지 Y좌표 (위도)를 String으로
+                mAppointmentData.longitude.toString(), // 도착지 (약속장소) X좌표 (경도)를 String
+                mAppointmentData.latitude.toString(),
+                null,
+                null,
+                null,
+                object : OnResultCallbackListener {
+                    override fun onSuccess(p0: ODsayData?, p1: API?) {
+                        // 길찾기 응답이 돌아오면 할 일.
+
+
+                    }
+
+                    override fun onError(p0: Int, p1: String?, p2: API?) {
+
+                    }
+
                 }
 
-            }
+            )
 
-            infoWindow.open(marker)
         }
     }
 }
