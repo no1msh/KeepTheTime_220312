2a37efaf9124b56099ff4bc1e97d282f252b2f76
TFQ_Moon <no1msh1217@gmail.com>
2022-03-26 11:10:39 +0900

    네이버 문서를 보고 , 네이버 지도의 정보창 띄우기

--- app/src/main/java/com/example/keepthetime_220312/ViewMapActivity.kt
+++ app/src/main/java/com/example/keepthetime_220312/ViewMapActivity.kt
@@ -5,6 +5,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_220312.databinding.ActivityViewMapBinding
 import com.example.keepthetime_220312.datas.AppointmentData
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.InfoWindow
+import com.naver.maps.map.overlay.Marker
 
 class ViewMapActivity : BaseActivity() {
 
@@ -43,6 +46,30 @@ class ViewMapActivity : BaseActivity() {
             val latLng = LatLng(mAppointmentData.latitude, mAppointmentData.longitude)
 
 //            지도 조작 코드
+
+            val cameraUpdate = CameraUpdate.scrollTo(latLng)
+
+            naverMap.moveCamera( cameraUpdate)
+
+            val marker = Marker()
+            marker.position = latLng
+            marker.map = naverMap
+
+//            정보창 띄우기
+
+            val infoWindow = InfoWindow()
+
+//            object : 추상클래스(생성자) = > 추상 클래스 객체를 담는다.
+//            object : 인터페이스 {    } = > 인터페이스는 생성자가 없이 사용한다.
+            infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext) {
+                override fun getText(p0: InfoWindow): CharSequence {
+//                    charSequence : String 으로 생각해도 무방.
+                    return "정보창 내용"
+                }
+
+            }
+
+            infoWindow.open(marker)
         }
     }
 }
